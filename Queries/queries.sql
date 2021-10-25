8cfa4c5f21071f914448249c52557df055159250
Josh Daniels <joshdaniels@Joshs-MBP.attlocal.net>
2021-10-24 20:21:05 -0500

    added retirement csv

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -17,14 +17,16 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+PRIMARY KEY (emp_no, dept_no)
 );
+
+DROP TABLE dept_manager CASCADE;
 
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
@@ -52,3 +54,23 @@ CREATE TABLE titles (
 );
 
 SELECT * FROM departments;
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
